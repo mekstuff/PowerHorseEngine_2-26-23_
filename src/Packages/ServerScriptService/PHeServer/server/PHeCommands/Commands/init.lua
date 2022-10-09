2d831bf8ca810af699a077b69aecf057a0aee289
LanzoInc <lanzoinc@gmail.com>
2022-10-09 13:17:10 -0400

    fix: fixed spelling mistake and check for CustomCMDS only if included

--- src/Packages/ServerScriptService/PHeServer/server/PHeCommands/Commands/init.lua
+++ src/Packages/ServerScriptService/PHeServer/server/PHeCommands/Commands/init.lua
@@ -1,13 +1,19 @@
 local App = require(game:GetService("ReplicatedStorage"):WaitForChild("PowerHorseEngine"));
 
 local CMDS = {}
-local function insetToCmds(p)
+local function insertToCmds(p)
 	for _,v in pairs(p:GetChildren())do if(v:IsA("ModuleScript"))then
 			table.insert(CMDS,require(v));
 		end;end;
 end;
-insetToCmds(script.cmds);
+insertToCmds(script.cmds);
 
-insetToCmds(App:GetGlobal("Engine"):RequestContentFolder().Config.CustomCMDS);
--- insetToCmds(game:GetService("ReplicatedStorage").PowerHorseEngine:WaitForChild("Content"):WaitForChild("Config").CustomCMDS)
+local Config = App:GetGlobal("Engine"):RequestContentFolder():FindFirstChild("Config");
+if(Config)then
+	local CustomCMDS = Config:FindFirstChild("CustomCMDS");
+	if(CustomCMDS)then
+		insertToCmds(CustomCMDS);
+	end
+end
+
 return CMDS;
