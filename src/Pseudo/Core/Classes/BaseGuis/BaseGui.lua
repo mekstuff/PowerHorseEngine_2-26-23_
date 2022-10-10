21a32510d32e952116e92bcb6b7fef60edcb8727
LanzoInc <lanzoinc@gmail.com>
2022-10-09 22:25:50 -0400

    fix: removed css property from basegui's

--- src/Pseudo/Core/Classes/BaseGuis/BaseGui.lua
+++ src/Pseudo/Core/Classes/BaseGuis/BaseGui.lua
@@ -28,14 +28,9 @@ local BaseGui = {
 	Visible = true;
 	ZIndex = 1;
 	Disabled = false;
-	css = "";
 	SupportsRBXUIBase = false;
 };
 BaseGui.__inherits = {}
---[=[
-@prop css String
-@within BaseGui
-]=]
 --[=[
 @prop Disabled Boolean
 @within BaseGui
@@ -123,114 +118,20 @@ function cssObjectClass:_Render()
 end
 --//
 function BaseGui:_init()
-	
-	local App = self:_GetAppModule();
-	local Content = App:GetGlobal("Engine").RequestContentFolder();
-	--local Config = Content.Config;
-	--if(not Config)then return end;
-	
-	local _css_mod = Content:FindFirstChild("css");
-	local _css = _css_mod and require(_css_mod);
-	
-	if(not _css)then
-		Core.tossError("Tried to apply BaseGui css with no .css file. PowerHorseEngine -> .content -> css");
-	end
-	local class = self.ClassName;
-	local css = self.css;
-	
-	
-	local Base = self:GetGUIRef();
-	
-	--local cssService = self:_GetAppModule():GetService("css");
-
-	local function useStylesheetIf(format,sheet,noerrorcatch)
-		local class_format = format..sheet;
-			
-		local stylesheet = (_css[class_format]);
-		if(not stylesheet)then
-			--local preset = cssService:getPreset();
-			local tosearch = _css_mod;
-			--if(preset)then tosearch = preset;end
-			stylesheet = tosearch:FindFirstChild(class_format);
-			if(stylesheet)then stylesheet = require(stylesheet);end;
-			tosearch=nil;
-		end
-		if(stylesheet)then
-			if(typeof(stylesheet) == "function")then
-				local CustomClassService = App:GetService("CustomClassService");
-				local Class = CustomClassService:Create(cssObjectClass,nil,{self})
-				stylesheet(Class)
-				return;
-			end;
-			
-			Core.tossWarn(("You're using legacy css format, please switch to using a function instead of a table.'%s'example:\n\n[\"%s\"] = function(cssObject)end;\n\n learn about cssObjects on our documentation.\n"):format(class_format,class_format))
-			--//Legacy
-			for prop,val in pairs(stylesheet) do
-				if (typeof(val) ~= "function") then
-					self[prop]=val;
-				end
-			end;
-			if(stylesheet.cmd)then
-				stylesheet.cmd(self);
-			end;
-		else
-			if(not noerrorcatch)then
-				Core.tossWarn(sheet.." is not an existing stylesheet. in your .css file, are you sure it's \""..format..""..sheet.."\"?")
-			end;		
-		end;
-	end
-		
-	--if(not self.ClassName:match("^PHe"))then
-		self._dev._baseGUI__cssChanged = self:GetPropertyChangedSignal("css"):Connect(function()
-			if(self.css ~= "")then
-				local allcss = self.css:split(",");
-				for _,cssClass in pairs(allcss)do
-					local stringed = cssClass:gsub("[%s,\"]+","");
-					useStylesheetIf(".",stringed);
-				end
-				--useStylesheetIf("#",self.css);
-			end
-		end);
-	--end;
-	-- local realParentRef
 	self._dev._baseGui_supportsRBXUIBase = self:GetPropertyChangedSignal("SupportsRBXUIBase"):Connect(function()
 		if(self.SupportsRBXUIBase)then
-			-- print(self.Parent,self.Name);
 			self:GetGUIRef().Parent = self.Parent and ( self.Parent:IsA("Pseudo") and getROBLOXInstance(self.Parent) or self.Parent);
-			-- local realParentRef = Instance.new("ObjectValue");
-			-- realParentRef.Name = "lanzoinc_rpbgr_$l";
-			-- realParentRef.Parent = self:GetGUIRef();
-			-- realParentRef.Value = self:GetRef();
 			self._dev._baseGui_supportsRBXUIBase_ParentChanged = self:GetPropertyChangedSignal("Parent"):Connect(function()
 				self:GetGUIRef().Parent = self:GetRef().Parent;
 			end);
-			-- local OriginalRefName = self:GetGUIRef().Name;
-			-- self:GetGUIRef().Name = OriginalRefName..self.Name;
-			-- self._dev._baseGui_supportsRBXUIBase_NameChanged = self:GetPropertyChangedSignal("Name"):Connect(function()
-				-- self:GetGUIRef().Name = OriginalRefName..self.Name;
-			-- end)
 		else
-			-- local _rpbgr = self:GetGUIRef():FindFirstChild("lanzoinc_rpbgr_$l");
-			-- if(_rpbgr)then
-				-- _rpbgr:Destroy();
-			-- end;
-			-- _rpbgr = nil;
 			self:GetGUIRef().Parent = self:GetRef();
 			if(self._dev._baseGui_supportsRBXUIBase_ParentChanged)then
 				self._dev._baseGui_supportsRBXUIBase_ParentChanged:Disconnect();
 				self._dev._baseGui_supportsRBXUIBase_ParentChanged=nil;
 			end;
-			--[[
-			if(self._dev._baseGui_supportsRBXUIBase_NameChanged)then
-				self._dev._baseGui_supportsRBXUIBase_NameChanged:Disconnect();
-				self._dev._baseGui_supportsRBXUIBase_NameChanged=nil;
-			end
-			]]
-		end
-		
+		end;
 	end)
-	--self._dev._p
-	--useStylesheetIf(".",self.ClassName,true);
 end;
 --//
 function BaseGui:CreateTween(TweenInfo_,TweenProps,ToTween)
