83b204ef04a067f2f0cae4fafd9c0fd998762301
LanzoInc <lanzoinc@gmail.com>
2022-11-02 00:08:19 -0400

    fix(Math library): You can pass only a min value and max will be the opposite of it

--- src/Core/Libraries/Math.lua
+++ src/Core/Libraries/Math.lua
@@ -8,7 +8,8 @@ local Math = {};
 local sin = math.sin;
 local pi = math.pi;
 
-function Math.oscillate(min:number,max:number,Time:number?):number
+function Math.oscillate(min:number,max:number?,Time:number?):number
+    max = max or (min < 0 and math.abs(min) or min-(min*2))
     Time = Time or time();
     local addv = (min + max)/2;
     local subv = (min - max)/2
