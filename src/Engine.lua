9e6fe206ba2cc575b3381cd20fb5ee24342a8b4d
LanzoInc <lanzoinc@gmail.com>
2022-11-15 18:28:27 -0500

    refactor(Engine): Cleaned up source

--- src/Engine.lua
+++ src/Engine.lua
@@ -1,6 +1,6 @@
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
--- local Util = script.Parent.Util;
+
 
 --[=[
 	@class Engine
@@ -38,65 +38,13 @@ local StorageEvents = {
 		"PHe_PanelRemover";
 		"MagicBuild_ExecuteBuild";
 		"CoreFunc_ClientEngineProvider";
+		"SSRENDERV1_SUBSCRIBEREQUEST";
 		--"KEYSERVICESHAREDKEYGRABBER";
 		
 	};["BindableEvent"]={
 		
 	};
 };
-
---[[
-local function EstablishGlobals()
-	local rs = game:GetService("ReplicatedStorage");
-	if(script.Parent.Parent ~= rs)then return end;
-	--print("Establishing Globals");
-	local Globals = script.Parent.Core.Globals;
-	for _,g in pairs(Globals:GetChildren()) do
-		_G[g.Name] = require(g);
-	end
-end;
-]]
---[[
-
-local function userConfig()
-
-	local Manifest = require(script.Parent["Manifest"]);
-	-- local Config = require(Engine:RequestContentFolder():FindFirstChild("Config"));
-	
-	
-	local ManifestVersion,ConfigVersion = Manifest.Upd.Version:gsub(" ",""),Config.v:gsub(" ","");
-	
-	if(ManifestVersion ~= ConfigVersion)then
-		warn("[PowerHorseEngine] Version mismatch. Your version -> "..ConfigVersion.." Server version -> "..ManifestVersion);
-		warn("Update PowerHorseEngine Installer from toolbox");
-		warn("\nVersion Notes:");
-		warn( (Manifest.Upd.VersionNotes.Major and #Manifest.Upd.VersionNotes.Major>0) and "Major Changes:\n"..table.concat(Manifest.Upd.VersionNotes.Major, "\n") or "No Major Changes.")
-		warn( (Manifest.Upd.VersionNotes.Minor and #Manifest.Upd.VersionNotes.Minor>0) and "Minor Changes:\n"..table.concat(Manifest.Upd.VersionNotes.Minor, "\n") or "No Minor Changes.")
-		warn( (Manifest.Upd.VersionNotes.Patches and #Manifest.Upd.VersionNotes.Patches>0) and "Patches:\n"..table.concat(Manifest.Upd.VersionNotes.Patches, "\n") or "No Patches.")
-		if(Manifest.Upd.UpdReq)then
-			script.Parent:Destroy();
-			error("Update required. PowerHorseEngine failed.");
-		end
-	end
-	
-	
-	local maniV1,maniV2,maniV3 = Manifest.Upd.Version:gsub(" ",""):match("([%d]+)%.(%d+)%.(%d+)");
-	local configv1,configv2,configv3 = Config.v:gsub(" ",""):match("([%d]+)%.(%d+)%.(%d+)");
-
-	print(maniV1,maniV2,maniV3)
-	
-	print(configv1,configv2,configv3)
-	
-	local outdated=false;
-	local levelOutdate = 0
-	
-	if(maniV3 > configv3)then outdated=true;levelOutdate=1 end;
-	if(maniV2 > configv2)then outdated=true;levelOutdate=2 end;
-	if(maniV1 > configv1)then outdated=true;levelOutdate=3 end;
-		
-end;
-]]
-
 
 function Engine:InitPlugin(plugin)
 
@@ -105,20 +53,6 @@ function Engine:InitPlugin(plugin)
 	local PluginService = ServiceProvider:LoadServiceAsync("PluginService");
 	PluginService:Sync(plugin);
 	print("\nRunning", Manifest.Name, "v-", Manifest.Upd.Version, "for plugin \"", plugin,"\"");
-	
-	--[[
-	local Manifest = require(script.Parent["Manifest"])
-	local CoreGui = game:GetService("CoreGui");
-	local PluginFolderCoreGui = CoreGui:FindFirstChild(Manifest.Name.."-PluginUtil");
-	
-	if(not PluginFolderCoreGui)then
-		local f = script.Parent.Util:FindFirstChild("Plugin");
-		f.Name = Manifest.Name.."-PluginUtil";
-		f.Parent = CoreGui;
-		require(f.init)();
-	end
-	]]
-	--local PluginService = 
 end
 
 function Engine:InitServer(PushPackages:boolean)	
@@ -183,20 +117,6 @@ function Engine:InitServer(PushPackages:boolean)
 		if(PushPackages)then
 			script.Parent.Packages.PackageController.Parent = game.ServerScriptService;
 		end
-		
-		-- script.Parent.Packages.PackageController.Parent = game:GetService("ServerScriptService");
-	-- local thread = coroutine.create(function()
-	-- 	if(PushPackages)then
-	-- 	    for _,Package in pairs(script.Parent.Packages:GetChildren())do
-	-- 			if(Package:IsA("Folder") and Package.Name ~= "$Preloaders")then
-	-- 				for _,x in pairs(Package:GetChildren())do
-	-- 					x.Parent = game:FindFirstChild(Package.Name);
-	-- 				end
-	-- 			end
-	-- 		end;
-	-- 	end
-	-- end);coroutine.resume(thread);
-	
 end;
 
 --//
@@ -204,7 +124,6 @@ function Engine:InitClient(Client)
 	if not Client then 
 		Client = game.Players.LocalPlayer;
 	end;
-	-- EstablishGlobals();
 end;
 
 local function waitForContentFolder(tries:number)
@@ -252,12 +171,9 @@ function Engine:FetchStorageEvent(EventName:string, Type:string)
 	if not Type then
 		Type = "RemoteEvents";
 	end;
-	--Type = Type.."Events";
-	
+
 	local PHe_RS_events = game:GetService("ReplicatedStorage"):WaitForChild("PHe_RS",600):WaitForChild("events",600);
-	
-	--local Search 
-	
+
 	local TypeSearch = PHe_RS_events:FindFirstChild(Type);
 	if not TypeSearch then
 		local x = Instance.new("Folder");
@@ -265,16 +181,14 @@ function Engine:FetchStorageEvent(EventName:string, Type:string)
 		x.Parent = PHe_RS_events;
 		TypeSearch = x;
 	end;
-	
+
 	local eventSearch = TypeSearch:FindFirstChild(EventName);
 	if not eventSearch and game:GetService("RunService"):IsServer() then
 		local t = string.sub(TypeSearch.Name, 1, #TypeSearch.Name-1);
-
 		local x = Instance.new(t);
 		x.Name = EventName;
 		x.Parent = TypeSearch;
 		eventSearch = x;
-		--print("Event : "..EventName.." was created because it didn't exist before.");
 	else
 		eventSearch = TypeSearch:WaitForChild(EventName);
 	end;
