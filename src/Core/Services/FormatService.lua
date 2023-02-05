5efe99cca1327220d3366c2c5329a5954749e2b2
LanzoInc <lanzoinc@gmail.com>
2023-02-05 11:57:52 -0400

    fix(FormatService): Formatting will now :End

--- src/Core/Services/FormatService.lua
+++ src/Core/Services/FormatService.lua
@@ -11,23 +11,23 @@ local FormatService = {}
 
 --[=[]=]
 function FormatService:toNumberAbbreviation(n:number,...:any):string
-	return Format(n):toNumberAbbreviation(...);
+	return Format(n):toNumberAbbreviation(...):End();
 end;
 --[=[]=]
 function FormatService:toNumberCommas(n:number,...:any):string
-	return Format(n):toNumberCommas(...);
+	return Format(n):toNumberCommas(...):End();
 end
 --[=[]=]
 function FormatService:toTimeFormat(timeStamp:number,is12Hour:boolean?):string
-	return Format(timeStamp):fromUnixStamp(is12Hour):toTimeFormat();
+	return Format(timeStamp):fromUnixStamp(is12Hour):toTimeFormat():End();
 end;
 --[=[]=]
 function FormatService:toDateFormat(timeStamp:number,useString:boolean?,shortenString:boolean?,indicateDayAsNumber:boolean?):string
-	return Format(timeStamp):fromUnixStamp(true):toDateFormat(useString,shortenString,indicateDayAsNumber);
+	return Format(timeStamp):fromUnixStamp(true):toDateFormat(useString,shortenString,indicateDayAsNumber):End();
 end;
 --[=[]=]
 function FormatService:toTimeDifference(t1:number,t2:number,...:any):string
-	return Format(t1,t2):toTimeDifference(...);
+	return Format(t1,t2):toTimeDifference(...):End();
 end
 
 
